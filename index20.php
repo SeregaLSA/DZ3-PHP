469a9d3d39e28072ce6ddae2f28073919dd687c9
SeregaLSA <vip.sergeylsa@mail.ru>
2023-04-07 23:11:42 +0600

    123

---
+++ index20.php
@@ -0,0 +1,63 @@
+<?php
+$year = [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+];
+echo "<pre>";
+print_r($year);
+echo "</pre>";
+
+$json = json_encode($year);
+echo($json);
+
+$year1 = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+
+echo "<pre>";
+print_r($year1);
+echo "</pre>";
+
+$json1 = json_encode($year1);
+echo($json1);
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+
+</body>
+
+</html>
